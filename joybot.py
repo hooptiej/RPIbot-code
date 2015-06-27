589c9b9fdd444413012fa6c5d1243c49fcca9833
Jason Kennedy <hooptiej@gmail.com>
2015-06-27 15:53:55 -0600

    socket troubleshooting

--- joybot.py
+++ joybot.py
@@ -8,7 +8,7 @@ import socket               # Import socket module
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 56565                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+s.bind(('', port))        # Bind to the port
 
 
 print(" ")
