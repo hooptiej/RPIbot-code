c7dcb8758a89b2dfc403170e252e9a97a1d3cca0
Jason Kennedy <hooptiej@gmail.com>
2015-06-27 23:09:09 -0600

    Add Servo Control
    Add Servo Control and confirm libs in place Cleanup

--- joybot.py
+++ joybot.py
@@ -1,15 +1,38 @@
 import time
 import os
 import pickle
-import pprint
+import Adafruit_I2C
+import Adafruit_PWM_Servo_Driver
+from Adafruit_PWM_Servo_Driver import PWM
 
-#Netcode?
+# Initialize the Servo Controller
+pwm = PWM(0x40)
+servoMin = 80  # Min pulse length out of 4096
+servoCenter = 330 # math center
+servoMax = 550  # Max pulse length out of 4096
+servoPos = servoCenter
+servopos = servoCenter
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print "%d us per period" % pulseLength
+  pulseLength /= 4096                     # 12 bits of resolution
+  print "%d us per bit" % pulseLength
+  pulse *= 1000
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
+pwm.setPWMFreq(60)    
+#end Servo controller init           
+
+
+
+#Initalize Netcode
 import socket               # Import socket module
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 56565                # Reserve a port for your service.
 s.bind(('', port))        # Bind to the port
-#data = s.recv(1024).decode()
+
 
 
 print(" ")
