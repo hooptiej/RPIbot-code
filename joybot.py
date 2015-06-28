b1cac9726e5d1423ff2af61566df27c260dd38e9
Jason Kennedy <hooptiej@gmail.com>
2015-06-27 23:10:24 -0600

    py 2-3

--- joybot.py
+++ joybot.py
@@ -15,9 +15,9 @@ servopos = servoCenter
 def setServoPulse(channel, pulse):
   pulseLength = 1000000                   # 1,000,000 us per second
   pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
+  #print "%d us per period" % pulseLength
   pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
+  #print "%d us per bit" % pulseLength
   pulse *= 1000
   pulse /= pulseLength
   pwm.setPWM(channel, 0, pulse)
