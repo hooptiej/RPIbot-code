b322a3b5e5aa2f5d711fcd370d53f8da653a644d
Jason Kennedy <hooptiej@gmail.com>
2015-08-29 20:40:37 -0600

    indents.

--- abipedtest.py
+++ abipedtest.py
@@ -70,9 +70,9 @@ else:
         clear()
         for event in pygame.event.get(): # User did something
             if event.type == pygame.JOYBUTTONDOWN:
-                    print("Joystick button pressed.")
-                if event.type == pygame.JOYBUTTONUP:
-                    print("Joystick button released.")
+                print("Joystick button pressed.")
+                    if event.type == pygame.JOYBUTTONUP:
+                        print("Joystick button released.")
             if event.type == pygame.JOYAXISMOTION:
                     print("Stick moving.")
             if event.type == pygame.JOYHATMOTION:
