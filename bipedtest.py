275ee1d4412616ef293211cbc1f9de031ec8a91b
Jason Kennedy <hooptiej@gmail.com>
2015-08-29 19:12:23 -0600

    sync legs

--- bipedtest.py
+++ bipedtest.py
@@ -160,11 +160,11 @@ while loop == 1:
 				#	servoposX = servoCenter
 					
 				if joyposone > 0:
-                    servoposX = servoposX - 3
+                    servoposX = servoposX + 3
                     servoposY = servoposY - 3
                 elif joyposone < 0:
                     servoposY = servoposY + 3
-					servoposX = servoposX + 3
+					servoposX = servoposX - 3
 				print " "
 				if servoposX >= servoMax:
 					servoposX = servoMax
