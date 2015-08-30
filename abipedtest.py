d10cf4bc1ca57eecd3f85ab8c905c6bc5e423636
Jason Kennedy <hooptiej@gmail.com>
2015-08-29 22:36:54 -0600

    as

--- abipedtest.py
+++ abipedtest.py
@@ -136,12 +136,12 @@ else:
                     rwaistpos = rwaistpos + 3
                     lfootpos = lfootpos - 3
                     rfootpos = rfootpos + 3
-            pwm.setPWM(lwaist, 0, lwaistpos)
-            pwm.setPWM(rwaist, 0, rwaistpos)
-            pwm.setPWM(lfoot, 0, lfootpos)
-            pwm.setPWM(rfoot, 0, rfootpos)
-            pwm.setPWM(lankle, 0, lanklepos)
-            pwm.setPWM(rankle, 0, ranklepos)
-            pwm.setPWM(lhip, 0, lhippos)
-            pwm.setPWM(rhip, 0, rhippos)
+                pwm.setPWM(lwaist, 0, lwaistpos)
+                pwm.setPWM(rwaist, 0, rwaistpos)
+                pwm.setPWM(lfoot, 0, lfootpos)
+                pwm.setPWM(rfoot, 0, rfootpos)
+                pwm.setPWM(lankle, 0, lanklepos)
+                pwm.setPWM(rankle, 0, ranklepos)
+                pwm.setPWM(lhip, 0, lhippos)
+                pwm.setPWM(rhip, 0, rhippos)
             clock.tick(15)
