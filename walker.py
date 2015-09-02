d057048337581f92494e6625f3ffc5f41093c1e2
Jason Kennedy <hooptiej@gmail.com>
2015-09-02 15:08:08 -0600

    new walker test soft

---
+++ walker.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+from Adafruit_PWM_Servo_Driver import PWM
+import time
+import os
+clear = lambda : os.system('tput reset')
+import pygame
+from pygame.locals import *
+clock = pygame.time.Clock()
+clear()
+# Initialise the PWM device using the default address
+pwm = PWM(0x40)
+# Note if you'd like more debug output you can instead run:
+#pwm = PWM(0x40, debug=True)
+
+servoMin = 210  # Min pulse length out of 4096
+servoCenter = 330 # math center
+servoMax = 450  # Max pulse length out of 4096
+
+# set servo channels
+lankle = 15
+rankle = 0
+
+lfoot = 13
+rfoot = 2
+
+lhip = 11
+rhip = 4
+
+rwaist = 9
+lwaist = 6
+
+ranklepos = servoCenter
+lanklepos = servoCenter
+rfootpos = servoCenter
+lfootpos = servoCenter
+rwaistpos = servoCenter
+lwaistpos = servoCenter
+rhippos = servoCenter
+lhippos = servoCenter
+
+
+def setServoPulse(channel, pulse):
+	pulseLength = 1000000                   # 1,000,000 us per second
+	pulseLength /= 60                       # 60 Hz
+	print "%d us per period" % pulseLength
+	pulseLength /= 4096                     # 12 bits of resolution
+	print "%d us per bit" % pulseLength
+	pulse *= 1000
+	pulse /= pulseLength
+	pwm.setPWM(channel, 0, pulse)
+
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+print " "
+print "Biped Test Control center V.a2"
+print " "
+print "Centering Servos"
+
+
+pwm.setPWM(lwaist, 0, lwaistpos)
+pwm.setPWM(rwaist, 0, rwaistpos)
+pwm.setPWM(lfoot, 0, lfootpos)
+pwm.setPWM(rfoot, 0, rfootpos)
+pwm.setPWM(lankle, 0, lanklepos)
+pwm.setPWM(rankle, 0, ranklepos)
+pwm.setPWM(lhip, 0, lhippos)
+pwm.setPWM(rhip, 0, rhippos)
+
